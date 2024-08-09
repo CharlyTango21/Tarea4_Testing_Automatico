5bdb0c3d967e93f7d6368fb61dfc0c86e56fe5c8
Carlos Torres <carlos.a.torres.vargas@gmail.com>
2024-08-08 21:01:20 -0400

    primera subida

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Evaluación 4: Testing automático</h1>
+      <div className="card">
+        <p className="read-the-docs">
+          Integrantes
+          <br />
+          Ignacio Delgadillo
+          <br />
+          Robert Collins
+          <br />
+          Jose Rojas
+          <br />
+          Carlos Torres
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
